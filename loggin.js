af9c44492ba5cfd8ec2bb2a4d68fadaa21fc820c
knightcoders18 <162946160+knightcoders18@users.noreply.github.com>
2024-03-12 08:12:36 +0530

    Add files via upload

---
+++ loggin.js
@@ -0,0 +1,7 @@
+const username = "admin";
+ const password = "password"; 
+ function login()
+  { const inputUsername = document.getElementById("username").value;
+   const inputPassword = document.getElementById("password").value; 
+   if (inputUsername === username && inputPassword === password) { window.location.href = "todolist.html"; } else { alert("Invalid username or password"); } } 
+document.getElementById("myButton").addEventListener("click", login); 
